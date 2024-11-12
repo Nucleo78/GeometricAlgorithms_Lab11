19aab1659e6a0c9f65af4cd9461be7a6e71c0da3
Yanis OULTAF <yanis.oultaf@gmail.com>
2024-11-12 17:35:23 +0100

    3.1 Color Utility Functions

---
+++ color.h
@@ -0,0 +1,28 @@
+//
+// Created by yanis on 12/11/2024.
+//
+
+#ifndef LAB_11_COLOR_H
+#define LAB_11_COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Translate the [0,1] component values to the byte range [0,255].
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    // Write out the pixel color components.
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif //LAB_11_COLOR_H
